408426ff97c0ccf908788037499453b6cbb2a19f
nonsense-j <3225910772@qq.com>
2022-12-14 07:05:24 +0800

    添加用户展示项目管理界面

--- pages/project/featmap.vue
+++ pages/project/featmap.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="d-flex flex-column">
-    <div class="d-flex flex-row align-start justify-center">
+    <div class="d-flex flex-row align-start justify-center mb-4">
       <v-btn elevation="4" class="text-btn " size="x-large" prepend-icon="mdi-swap-vertical-bold"
         append-icon="mdi-swap-vertical-bold" variant="outlined">
         进度管理
       </v-btn>
     </div>
-    <v-sheet>
-
+    <v-sheet v-for="(fName, fIndex) in featureMap.featureNames" :key="fIndex" class="d-flex flex-column py-8 px-4">
+      <v-btn size="large" color="primary" class="text-btn">{{ fName }}</v-btn>
+      <div class="mt-6">
+        <v-timeline direction="horizontal">
+          <v-timeline-item v-for="(feature, index) in featureMap.featureInfos[fIndex]" :key="index"
+            icon="mdi-map-marker-star" fill-dot dot-color="white">
+            <div class="d-flex flex-column justify-center align-center">
+              <div class="mb-2">
+                <v-chip v-for="editor in feature.editors" class="ml-1" color="info" label size="small">
+                  {{ editor }}
+                </v-chip>
+              </div>
+              <v-responsive class="border py-2 px-2">
+                {{ feature.msg }}
+              </v-responsive>
+            </div>
+          </v-timeline-item>
+        </v-timeline>
+      </div>
     </v-sheet>
   </div>
 </template>
@@ -47,25 +64,49 @@ const user = {
 // record接口 type(1学生记录，2老师记录)，按照时间顺序由近到远
 const featureMap = reactive({
   groupID: route.query.id,
-
-  features: [
-    {
-      time: '2022-10-12-22:00',
-      type: 1, // 1表示学生提交，2表示老师回复
-      fileNames: ["第一版开题报告.doc", "附属说明.doc",],
-      fileUrls: ["url1", "url2"],
-      msg: "报告介绍了项目的业务场景，进行了相关领域痛点分析，并且划分了基础功能以及扩展功能。"
-    }, {
-      time: '2022-10-14-12:00',
-      type: 2,
-      advices: ["关于业务场景部分应该要注意对比", "基础功能的实施安排应该再详细一点"]
-    }, {
-      time: '2022-10-18-20:18',
-      type: 1, // 1表示学生提交，2表示老师回复
-      fileNames: ["第一版开题报告.doc"],
-      fileUrls: ["url1"],
-      msg: "已按照老师要求完成修改。"
-    },
+  count: 3,
+  featureNames: ["登陆注册实现", "文件上传下载管理", "页面路由权限划分"],
+  featureInfos: [[{
+    time: '2022-10-12',
+    editors: ["Aurora", "Charlie"],
+    msg: "第一阶段实现：环境部署"
+  }, {
+    time: '2022-10-20',
+    editors: ["Aurora"],
+    msg: "完成前端设计"
+  }, {
+    time: '2022-11-18',
+    editors: ["Charlie"],
+    msg: "完成后端接口设计"
+  }], [{
+    time: '2022-10-26',
+    editors: ["Aurora", "Charlie"],
+    msg: "环境部署完成"
+  }, {
+    time: '2022-11-1',
+    editors: ["Aurora"],
+    msg: "获取到了阿里云的OSS API"
+  }, {
+    time: '2022-11-20',
+    editors: ["Aurora"],
+    msg: "文件管理demo测试通过"
+  }, {
+    time: '2022-12-10',
+    editors: ["Charlie"],
+    msg: "前后端交互组件完成"
+  }], [{
+    time: '2022-10-4',
+    editors: ["Aurora", "Charlie"],
+    msg: "环境部署完成"
+  }, {
+    time: '2022-10-27',
+    editors: ["Aurora"],
+    msg: "demo样例跑通"
+  }, {
+    time: '2022-11-23',
+    editors: ["Charlie"],
+    msg: "迁移成功"
+  }],
   ]
 }
 );
